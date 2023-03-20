1fac7622aa9e0da294c2b43c6473c6df55afa2fb
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2023-03-20 22:03:22 +0530

    Inheritance added

--- src/C_NonPrimitiveDataType/C_Class/ClassIntro.java
+++ src/C_NonPrimitiveDataType/C_Class/ClassIntro.java
@@ -1,4 +1,59 @@
 package C_NonPrimitiveDataType.C_Class;
 
+class c2{
+	
+	public void m1() {
+		System.out.println("Method1 of c2 called");
+	}
+	
+	public void m2() {
+		System.out.println("Method2 of c2 called");
+	}
+	
+	public int  m3() {
+		System.out.println("Method3 of c2 called returning 1");
+		return 1;
+	}
+	
+	int x=m3();
+	
+	public void m4() {
+		System.out.println("Method4 of c2 called");
+		System.out.println("X= "+x);
+	}
+	
+	//m1();
+	
+	public static void m5() {
+		System.out.println("Static Method5 of c2 called");
+	}
+}
+
 public class ClassIntro {
+	
+	static void method1() {
+		System.out.println("Method1 called");
+	}
+	
+	static void method2() {
+		System.out.println("Method2 called");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Main method called..");
+		method1();
+		method2();
+		
+		/**********************************************************************************/
+		
+		c2.m5();
+		
+		c2 c2=new c2();
+		c2.m1();
+		c2.m2();
+		c2.m4();
+		
+		
+	}
 }
--- src/F_OOPS/C_Inheritencs/Example1.java
+++ src/F_OOPS/C_Inheritencs/Example1.java
@@ -1,4 +1,71 @@
 package F_OOPS.C_Inheritencs;
 
+class parent1 {
+
+	public void method1() {
+		System.out.println("Public method1 of parent called.");
+	}
+
+	public void method2() {
+		System.out.println("Public method2 of parent called.");
+	}
+
+	private void method3() {
+		System.out.println("Private method3 of parent called.");
+	}
+
+	final void method4() {
+		System.out.println("Final method4 of parent is called.");
+	}
+
+}
+
+class child extends parent1 {
+
+	public void method2() {
+		System.out.println("Public method2 of parent is override and called by child.");
+	}
+
+//	public void method4() {
+//		System.out.println("Final method can not be override");
+//	}
+
+	public void method5() {
+		System.out.println("Public method5 of child  called.");
+	}
+}
+
 public class Example1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("********************************************************************");
+		System.out.println("Parent object");
+		
+		parent1 parent1=new parent1();
+		parent1.method1();
+		parent1.method2();
+		//parent1.method3();
+		parent1.method4();
+		
+		System.out.println("********************************************************************");
+		System.out.println("Child object");
+		
+		child child =new child();
+		child.method1();
+		child.method2();
+		child.method4();
+		child.method5();
+		
+		System.out.println("********************************************************************");
+		System.out.println("Parent object from child");
+		
+		parent1 parent2 =new child();
+		parent2.method1();
+		parent2.method2();
+		//parent2.method3();
+		parent2.method4();
+	    //parent2.method5();
+	}
+
 }
